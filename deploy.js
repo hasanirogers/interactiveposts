23f6a423dfe5b725fa3ec26e0a3831d81efb3ca3
Hasani Rogers <hasani.rogers@gmail.com>
2022-09-05 13:23:48 -0400

    added deploy workflow

---
+++ deploy.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-console */
+const path = require('path');
+const SftpClient = require('ssh2-sftp-client');
+
+require('dotenv').config();
+
+const config = {
+  host: process.env.FTP_DEPLOY_HOST,
+  username: process.env.FTP_DEPLOY_USERNAME,
+  password: process.env.FTP_DEPLOY_PASSWORD,
+  port: process.env.FTP_DEPLOY_PORT || 22,
+};
+
+const isDev = process.argv.indexOf('--dev') > -1;
+
+const directories = {
+  themes: {
+    local: '/wp-content/themes',
+    remote: isDev ? '/var/www/ippm.hasanirogers.me/public_html/wp-content/themes' : '/var/www/interactiveposts.dev/public_html/wp-content/themes',
+  },
+  plugins: {
+    local: '/wp-content/plugins',
+    remote: isDev ? '/var/www/ippm.hasanirogers.me/public_html/wp-content/plugins' : '/var/www/interactiveposts.dev/public_html/wp-content/plugins',
+  },
+  filter: /^(?!.*(.git|.github|node_modules))/gm
+};
+
+const upload = async () => {
+  const client = new SftpClient();
+  const themesSRC = path.join(__dirname, directories.themes.local);
+  const pluginsSRC = path.join(__dirname, directories.plugins.local);
+
+  try {
+    await client.connect(config);
+
+    client.on('upload', (info) => {
+      console.log(`Listener: Uploaded ${info.source}`);
+    });
+
+    const options = {
+      filter: (path, isDirectory) => directories.filter.test(path)
+    }
+
+    await client.uploadDir(themesSRC, directories.themes.remote, options);
+    await client.uploadDir(pluginsSRC, directories.plugins.remote, options);
+
+    return;
+  } finally {
+    client.end();
+  }
+};
+
+upload()
+  .then(message => console.log(message))
+  .catch((error) => { console.log(`main error: ${error.message}`); });
